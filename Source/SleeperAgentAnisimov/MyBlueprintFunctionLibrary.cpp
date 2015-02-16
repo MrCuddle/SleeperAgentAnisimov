0a977ccf8a4c3f24e23cc44e8e76d0ff757e3476
Jesper Sahlin <jesper.sahlin@gmail.com>
2015-02-16 10:53:14 +0100

    Fixed camera position

---
+++ Source/SleeperAgentAnisimov/MyBlueprintFunctionLibrary.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SleeperAgentAnisimov.h"
+#include "MyBlueprintFunctionLibrary.h"
+
+
+FText UMyBlueprintFunctionLibrary::Test()
+{
+	return FText::FromString("HEJ SIMON");
+}
+
