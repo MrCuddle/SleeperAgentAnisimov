3903f62cbaa56cdf1535301d2e9033f9ac5cd776
Victor <victor.olsson123@gmail.com>
2015-03-06 14:56:40 +0100

    hej

---
+++ Source/SleeperAgentAnisimov/SleeperAgentAnisimovGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SleeperAgentAnisimovGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLEEPERAGENTANISIMOV_API ASleeperAgentAnisimovGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+		
+};
+
