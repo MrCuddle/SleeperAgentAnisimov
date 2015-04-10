d5212038d2f2d4ee652ff436ca141c7f24b71e8c
Jesper Sahlin <jesper.sahlin@gmail.com>
2015-04-10 17:56:36 +0200

    Logging stuff

--- Source/SleeperAgentAnisimov/LoggingFunctions.cpp
+++ Source/SleeperAgentAnisimov/LoggingFunctions.cpp
@@ -3,16 +3,79 @@
 #include "SleeperAgentAnisimov.h"
 #include "LoggingFunctions.h"
 
-const string ULoggingFunctions::outputDir = "LOGGING";
+const string ULoggingFunctions::RootDirectory = "LOGGING";
 const string ULoggingFunctions::versionFormat = "v";
 const string ULoggingFunctions::fileEnding = ".log";
+string ULoggingFunctions::SessionDirectory = string();
 bool ULoggingFunctions::newSession = true;
+bool ULoggingFunctions::sessionInitialized = false;
 int ULoggingFunctions::versionNumber = 0;
 
-bool ULoggingFunctions::SaveToFile_SaveStringTextToFile(FString fileName, FString SaveText, FString& Result){
+void ULoggingFunctions::CreateLoggingFile(string fileName, string labels)
+{
+	time_t now = time(0);
+	char* date = ctime(&now);	
+	ofstream stream;
+	stream.open(fileName + fileEnding, ios_base::app);
+	stream << date << '\n';
+	stream << labels << '\n';
+	stream.close();
+}
+
+bool ULoggingFunctions::StartNewSession()
+{
+	time_t now = time(0);
+	char* date = ctime(&now);
+	string dateFormatted = date;
+	dateFormatted.erase(remove_if(dateFormatted.begin(), dateFormatted.end(), isspace), dateFormatted.end());
+	dateFormatted.erase(remove(dateFormatted.begin(), dateFormatted.end(), ':'), dateFormatted.end());
+
+	FString p = FPaths::GameDir();
+	p += RootDirectory.c_str();
+	string path = string(TCHAR_TO_UTF8(*p));
+	path += "/" + dateFormatted;
+	FString pathTest = path.c_str();
+
+	if (FPlatformFileManager::Get().GetPlatformFile().DirectoryExists(*pathTest)){
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "LOGGING: Path: '" + pathTest + "' already exists. Logging session NOT started");
+		return false;
+	}
+	FPlatformFileManager::Get().GetPlatformFile().CreateDirectory(*pathTest);
+	if (!FPlatformFileManager::Get().GetPlatformFile().DirectoryExists(*pathTest)){
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "LOGGING: Could not create path: '" + pathTest + "'. Logging session NOT started");
+		return false;
+	}
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, "LOGGING: Created path: '" + pathTest + "', logging session started");
+	
+	//Create files
+	CreateLoggingFile(path + "/Health", "Time\tInstigator\tLocation");
+	CreateLoggingFile(path + "/Deaths", "Time\tLocation");
+	CreateLoggingFile(path + "/ItemPickups", "Time\tItem\tLocation");
+
+	sessionInitialized = true;
+	SessionDirectory = path;
+	return true;
+}
+
+bool ULoggingFunctions::SaveStringToFile(FString fileName, FString SaveText, FString& Result){
 	FString path;
 	path = FPaths::GameDir();
-	path += outputDir.c_str();
+	path += RootDirectory.c_str();
+	if (!sessionInitialized)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "LOGGING: No Session Started before logging. No data will be logged");
+		Result = "FAIL";
+		return false;
+	}
+
+	if (newSession)
+	{
+		time_t now = time(0);
+		char* date = ctime(&now);
+		path += "/";
+		path += date;
+	}
 
 	//check if directory exists. If not: create it
 	if (!FPlatformFileManager::Get().GetPlatformFile().DirectoryExists(*path)){
@@ -49,12 +112,70 @@ bool ULoggingFunctions::SaveToFile_SaveStringTextToFile(FString fileName, FStrin
 		stream << dt << '\n';
 	}
 	string data(TCHAR_TO_UTF8(*SaveText));
-	stream << data;
+	stream << data << '\n';
 	stream.close();
 	
 	Result = path + " : " + SaveText;
 	return true;
 }
 
+void ULoggingFunctions::SaveLog(string fileName, string data)
+{
+	ofstream stream;
+	stream.open(SessionDirectory + '/' + fileName + fileEnding, ios_base::app);
+	stream << data << '\n';
+	stream.close();
+}
 
+bool ULoggingFunctions::LogActorHealth(AActor* actor, AActor* instigator, int32 health, FString& Result)
+{	
+	if (actor == NULL)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "LOGGING: LogActorHealth FAILED");
+		return false;
+	}
+	string data = to_string(GetElapsedTime(actor));
+	data += '\t' + to_string(health) + '\t';
+	data += (instigator != NULL) ? TCHAR_TO_UTF8(*instigator->GetName()) : "NONE";
+	data += '\t';
+	FVector location = actor->GetActorLocation();
+	data += TCHAR_TO_UTF8(*location.ToString());
+	SaveLog("Health", data);
+	Result = data.c_str();
+	return true;
+}
 
+bool ULoggingFunctions::LogPlayerDeath(AActor* player, FString& Result)
+{	
+	if (player == NULL)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "LOGGING: LogPlayerDeath FAILED");
+		return false;
+	}
+	string data = to_string(GetElapsedTime(player));
+	data += '\t';
+	FVector location = player->GetActorLocation();
+	data += TCHAR_TO_UTF8(*location.ToString());
+	SaveLog("Death", data);
+	Result = data.c_str();
+	return true;
+}
+
+bool ULoggingFunctions::LogItemPickup(AActor* player, AItem* item, FString& Result)
+{
+	if (player == NULL)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "LOGGING: LogItemPickup FAILED");
+		return false;
+	}
+	
+	string data = to_string(GetElapsedTime(player));
+	data += '\t';
+	data += (item != NULL) ? TCHAR_TO_UTF8(*item->GetName()) : "NONE";
+	data += '\t';
+	FVector location = player->GetActorLocation();
+	data += TCHAR_TO_UTF8(*location.ToString());
+	SaveLog("ItemPickups", data);
+	Result = data.c_str();
+	return true;
+}
--- Source/SleeperAgentAnisimov/LoggingFunctions.h
+++ Source/SleeperAgentAnisimov/LoggingFunctions.h
@@ -3,9 +3,11 @@
 #pragma once
 
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "Item.h"
 #include <string>
 #include <fstream>
 #include <ctime>
+#include <algorithm>
 using namespace std;
 #include "LoggingFunctions.generated.h"
 
@@ -19,17 +21,37 @@ class SLEEPERAGENTANISIMOV_API ULoggingFunctions : public UBlueprintFunctionLibr
 	GENERATED_BODY()
 	
 public:
+	UFUNCTION(BlueprintCallable, meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "StartNewSession"), Category = "StaniLogging")
+		static bool StartNewSession();
 
-	UFUNCTION(BlueprintCallable, meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "SaveToFile"), Category = "LogginToolsForStani")
-		static bool SaveToFile_SaveStringTextToFile(FString fileName, FString SaveText, FString& Result);
+	UFUNCTION(BlueprintCallable, meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "SaveToFile"), Category = "StaniLogging")
+		static bool SaveStringToFile(FString fileName, FString SaveText, FString& Result);
+
+	UFUNCTION(BlueprintCallable, meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "LogPlayerHealth"), Category = "StaniLogging")
+		static bool LogActorHealth(AActor* actor, AActor* instigator, int32 health, FString& Result);
+
+	UFUNCTION(BlueprintCallable, meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "LogPlayerDeath"), Category = "StaniLogging")
+		static bool LogPlayerDeath(AActor* player, FString& Result);
+
+	UFUNCTION(BlueprintCallable, meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject", FriendlyName = "LogItemPickup"), Category = "StaniLogging")
+		static bool LogItemPickup(AActor* player, AItem* item, FString& Result);
 	
 private:
 
-	static const string outputDir;
+	inline static float GetElapsedTime(UWorld* contex) { return contex->GetTimeSeconds(); }
+	inline static float GetElapsedTime(AActor* contexHolder) { return contexHolder->GetWorld()->GetTimeSeconds(); }
+
+	static void SaveLog(string fileName, string data);
+
+	static void CreateLoggingFile(string fileName, string labels);
+
+	static const string RootDirectory;
 	static const string versionFormat;
 	static const string fileEnding;
 	static string version;
+	static string SessionDirectory;
 	static bool newSession;
+	static bool sessionInitialized;
 	static int versionNumber;
 
 
